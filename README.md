d64e3a6cbfd59eff7a8f10c40686c4b29804080f
alichen <ali322@gmail.com>
2017-09-06 14:40:14 +0800

    project start

---
+++ README.md
@@ -0,0 +1,37 @@
+progress-webpack-plugin [![npm version](https://badge.fury.io/js/progress-webpack-plugin.svg)](https://badge.fury.io/js/progress-webpack-plugin)
+===
+[![NPM](https://nodei.co/npm/progress-webpack-plugin.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/progress-webpack-plugin/)
+
+inspired by [progress-bar-webpack-plugin](https://github.com/clessg/progress-bar-webpack-plugin),simple Webpack plugin that display nice progress when build
+
+Install
+===
+
+```javascript
+npm install progress-webpack-plugin --save--dev
+```
+
+Usage
+===
+
+add plugin in your webpack.config.js
+
+```javascript
+var ProgressPlugin = require('progress-webpack-plugin')
+
+module.exports = {
+    ...
+    plugins:[
+        new ProgressPlugin(true)
+    ]
+}
+```
+
+Plugin Options
+===
+
+- **minimal**: enable minimal mode or not,default value is false
+
+## License
+
+[MIT License](http://en.wikipedia.org/wiki/MIT_License)
---
+++ index.js
@@ -0,0 +1,106 @@
+let chalk = require('chalk')
+let ProgressPlugin = require('webpack/lib/ProgressPlugin')
+let log = require('log-update')
+let figures = require('figures')
+let path = require('path')
+
+function now(){
+    return new Date().toTimeString().split(' ')[0]
+}
+
+function progressPlugin(minimal = false) {
+    let rootPath = path.resolve('.')
+    let prevStep = 0
+    let subPercentage
+    let startTime
+    let finishTime
+    let duration
+
+    return new ProgressPlugin((percentage, message, moduleProgress, activeModules, moduleName) => {
+        if (prevStep === 0) {
+            startTime = Date.now()
+        }
+        let output = minimal ? [chalk.yellow(`[${Math.round(percentage * 100)}%] `)] : []
+
+        // 1. compile
+        if (percentage >= 0 && percentage < 0.1) {
+            if (prevStep > 1) return
+            prevStep = 1
+
+            output.push(chalk.white(minimal ? 'Compile modules...' : `${figures.pointer} Compile modules`))
+        }
+        if (percentage === 0.1 && !minimal) {
+            output.push(`${figures.tick} Compile modules`)
+        }
+        // 2. build
+        if (percentage >= 0.1 && percentage <= 0.7) {
+            if (prevStep > 2) return
+            prevStep = 2
+
+            subPercentage = Math.round((percentage - 0.1) * 10000 / 60)
+            output.push(chalk.white(minimal ? 'Build modules...' : `${figures.pointer} Build modules (${subPercentage}%)`))
+
+            if (moduleName !== undefined) {
+                let betterModuleName = moduleName
+                // remove all details about used loaders
+                if (betterModuleName.indexOf('!') !== -1) {
+                    let splitModuleNames = betterModuleName.split('!')
+                    betterModuleName = splitModuleNames[splitModuleNames.length - 1]
+                }
+                // transform to relative path
+                if (betterModuleName.indexOf(rootPath) !== -1) {
+                    betterModuleName = betterModuleName.split(rootPath)[1].substring(1)
+                }
+                // improve path
+                // betterModuleName = betterModuleName.replace(/\\/g, '/').replace('./', '').replace('muti ', '')
+
+                let [current, total] = moduleProgress.split('/')
+                let moduleDetails = `${current} of ${total} :: ${betterModuleName}`
+                output.push(chalk.grey(minimal ? `(${moduleDetails})` : `  ${figures.arrowRight} ${moduleDetails}`))
+            }
+        }
+        if (percentage > 0.7 && !minimal) {
+            output.push(`${figures.tick} Build modules`)
+        }
+
+        // 3. optimize
+        if (percentage > 0.7 && percentage < 0.95) {
+            if (prevStep > 3) return
+            prevStep = 3
+            subPercentage = Math.round((percentage - 0.71) * 10000 / 23)
+            output.push(chalk.white(minimal ? 'Optimize modules...' : `${figures.pointer} Optimize modules (${subPercentage}%)`))
+            let extraMsg = message + percentage === 0.91 ? ' -- may take long time' : ''
+            output.push(chalk.grey(minimal ? extraMsg : `  ${figures.arrowRight} ${extraMsg}`))
+        }
+        if (percentage >= 0.95 && !minimal) {
+            output.push(`${figures.tick} Optimize modules`)
+        }
+
+        // 4. emmit
+        if (percentage >= 0.95 && percentage < 1) {
+            if (prevStep > 4) return
+            prevStep = 4
+            output.push(chalk.white(minimal ? 'Emmit files...' : `  ${figures.pointer} Emmit files`))
+        }
+        if (percentage === 1 && !minimal) {
+            output.push(`${figures.tick} Emmit files`)
+        }
+
+        // 5. finished
+        if (percentage === 1) {
+            prevStep = 0
+            finishTime = Date.now()
+            duration = (finishTime - startTime) / 1000
+            duration = duration.toFixed(3)
+
+            output.push(chalk.green(`Build successfully at ${now()} by ${duration}s`))
+        }
+
+        log(output.join(minimal ? '' : '\n'))
+        if (percentage === 1) {
+            log.done()
+        }
+    })
+}
+
+module.exports = progressPlugin
