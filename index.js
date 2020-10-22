77d51e7a8683e4508a2269c20f3fe8d7cad5e4a5
alichen <ali322@gmail.com>
2020-10-22 14:57:42 +0800

    compatible with webpack@5

--- index.js
+++ index.js
@@ -1,183 +1,110 @@
-let chalk = require('chalk')
-let ProgressPlugin = require('webpack/lib/ProgressPlugin')
-let log = require('log-update')
-let figures = require('figures')
-let path = require('path')
+let chalk = require("chalk");
+let ProgressPlugin = require("webpack/lib/ProgressPlugin");
+let log = require("log-update");
+let path = require("path");
+
+const thresholder = 0.99;
 
 function now() {
-  return new Date().toTimeString().split(' ')[0]
+  return new Date().toTimeString().split(" ")[0];
 }
 
 class Progress {
   constructor(options) {
-    this.delegate = new ProgressPlugin(options)
+    this.delegate = new ProgressPlugin(options);
   }
   apply(compiler) {
-    this.delegate.apply(compiler)
+    this.delegate.apply(compiler);
     let invalid = () => {
-      console.log(chalk.white('Compiling...'))
-    }
+      console.log(chalk.white("Compiling..."));
+    };
     if (compiler.hooks) {
-      compiler.hooks.invalid.tap('ProgressWebpckPlugin', invalid)
+      compiler.hooks.invalid.tap("ProgressWebpckPlugin", invalid);
     } else {
-      compiler.plugin('invalid', invalid)
+      compiler.plugin("invalid", invalid);
     }
   }
 }
 
-function progressPlugin(minimal = false, options = {}) {
-  let rootPath = path.resolve('.')
-  let identifier = options.identifier || ''
-  let id = identifier && identifier + ' '
-  let onStart = options.onStart || (() => {})
-  let onFinish = options.onFinish
-  let onProgress = options.onProgress
-  let clear = typeof options.clear === 'boolean' ? options.clear : true
-  let prevStep = 0
-  let subPercentage
-  let startTime
-  let finishTime
-  let duration
+function progressPlugin(formatted = true, options = {}) {
+  let identifier = options.identifier || "";
+  let id = identifier && identifier + " ";
+  let onStart = options.onStart || (() => {});
+  let onFinish = options.onFinish;
+  let onProgress = options.onProgress;
+  let clear = typeof options.clear === "boolean" ? options.clear : true;
+  let startTime;
+  let finishTime;
+  let duration;
 
-  return new Progress(
-    (percentage, message, moduleProgress, activeModules, moduleName) => {
-      if (prevStep === 0) {
-        startTime = Date.now()
-      }
-      let output = minimal
-        ? [chalk.yellow(`[${Math.round(percentage * 100)}%] `)]
-        : []
+  const handler = (percentage, message, ...args) => {
+    startTime = Date.now();
+    let output = formatted
+      ? [chalk.yellow(`[${Math.round(percentage * 100)}%] `)]
+      : [];
+    if (percentage > thresholder) {
+      return;
+    }
+    if (percentage >= 0 && percentage < thresholder) {
+      if (percentage === 0) onStart();
 
-      // 1. compile
-      if (percentage >= 0 && percentage < 0.1) {
-        if (prevStep > 1) return
-        prevStep = 1
-
-        if (percentage === 0) onStart()
-
-        output.push(
-          chalk.white(
-            minimal
-              ? `Compile ${id}modules...`
-              : `${figures.pointer} Compile ${id}modules`
-          )
-        )
-      }
-      if (percentage === 0.1 && !minimal) {
-        output.push(`${figures.tick} Compile ${id}modules`)
-      }
-      // 2. build
-      if (percentage >= 0.1 && percentage <= 0.7) {
-        if (prevStep > 2) return
-        prevStep = 2
-
-        subPercentage = Math.round((percentage - 0.1) * 10000 / 60)
-        output.push(
-          chalk.white(
-            minimal
-              ? `Build ${id}modules...`
-              : `${figures.pointer} Build ${id}modules (${subPercentage}%)`
-          )
-        )
-
-        if (moduleName !== undefined) {
-          let betterModuleName = moduleName
-          // remove all details about used loaders
-          if (betterModuleName.indexOf('!') !== -1) {
-            let splitModuleNames = betterModuleName.split('!')
-            betterModuleName = splitModuleNames[splitModuleNames.length - 1]
-          }
-          // transform to relative path
-          if (betterModuleName.indexOf(rootPath) !== -1) {
-            betterModuleName = betterModuleName.split(rootPath)[1].substring(1)
-          }
-          // improve path
-          // betterModuleName = betterModuleName.replace(/\\/g, '/').replace('./', '').replace('muti ', '')
-
-          let [current, total] = moduleProgress.split('/')
-          let moduleDetails = `${current} of ${total} ${betterModuleName}`
-          output.push(
-            chalk.grey(
-              minimal
-                ? `(${moduleDetails})`
-                : `  ${figures.arrowRight} ${moduleDetails}`
-            )
-          )
+      output.push(chalk.white(`${message} ${id}`));
+      if (args.length > 0) {
+        let details = args.join(" ");
+        if (
+          /^\d+\/\d+\s{1}modules/.test(args[0]) === true &&
+          args.length === 2
+        ) {
+          const rootPath = path.resolve(".");
+          details = [args[0]].concat([args[1].replace(rootPath, "")]).join(" ");
         }
+        output.push(chalk.grey(`(${details})`));
       }
-      if (percentage > 0.7 && !minimal) {
-        output.push(`${figures.tick} Build ${id}modules`)
+      if (!formatted) {
+        console.log(output.join("\n"));
+        return;
       }
+    }
 
-      // 3. optimize
-      if (percentage > 0.7 && percentage < 0.95) {
-        if (prevStep > 3) return
-        prevStep = 3
-        subPercentage = Math.round((percentage - 0.71) * 10000 / 23)
-        output.push(
-          chalk.white(
-            minimal
-              ? `Optimize ${id}modules...`
-              : `${figures.pointer} Optimize ${id}modules (${subPercentage}%)`
-          )
-        )
-        let extraMsg =
-          message + percentage === 0.91 ? ' -- may take long time' : ''
-        output.push(
-          chalk.grey(minimal ? extraMsg : `  ${figures.arrowRight} ${extraMsg}`)
-        )
-      }
-      if (percentage >= 0.95 && !minimal) {
-        output.push(`${figures.tick} Optimize ${id}modules`)
-      }
+    // // 5. finished
+    if (percentage === thresholder) {
+      finishTime = Date.now();
+      duration = (finishTime - startTime) / 1000;
+      duration = duration.toFixed(3);
 
-      // 4. emmit
-      if (percentage >= 0.95 && percentage < 1) {
-        if (prevStep > 4) return
-        prevStep = 4
-        output.push(
-          chalk.white(
-            minimal
-              ? `Emmit ${id}files...`
-              : `  ${figures.pointer} Emmit ${id}files`
-          )
-        )
-      }
-      if (percentage === 1 && !minimal) {
-        output.push(`${figures.tick} Emmit ${id}files`)
-      }
-
-      // 5. finished
-      if (percentage === 1) {
-        prevStep = 0
-        finishTime = Date.now()
-        duration = (finishTime - startTime) / 1000
-        duration = duration.toFixed(3)
-
-        if (typeof onFinish === 'function') {
-          onFinish(id, now(), duration)
-        } else {
-          output.push(
-            chalk.white(`Build ${id}finished at ${now()} by ${duration}s`)
-          )
-        }
-      }
-      if (onProgress) {
-        if (percentage > 0 && percentage < 1) {
-          onProgress(percentage)
-        }
+      if (typeof onFinish === "function") {
+        onFinish(id, now(), duration);
       } else {
-        log(output.join(minimal ? '' : '\n'))
-        if (percentage === 1) {
-          if (clear) {
-            log.clear()
-          } else {
-            log.done()
-          }
+        output.push(
+          chalk.white(`Build ${id}finished at ${now()} by ${duration}s`)
+        );
+      }
+    }
+    if (onProgress) {
+      if (percentage > 0 && percentage < thresholder) {
+        onProgress(percentage);
+      }
+    } else {
+      log(output.join(""));
+      if (percentage === thresholder) {
+        if (clear) {
+          log.clear();
+        } else {
+          log.done();
         }
       }
     }
-  )
+  };
+  return new Progress({
+    handler,
+    entries: false,
+    activeModules: false,
+    modules: true,
+    modulesCount: 5000,
+    dependencies: false,
+    dependenciesCount: 10000,
+    percentBy: "entries",
+  });
 }
 
-module.exports = progressPlugin
+module.exports = progressPlugin;
